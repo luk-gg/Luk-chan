b63c0f9ce2e0e70da64a6006bbfbb8779dbaf061
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2025-09-28 04:01:43 -0300

    refactor: Simplify cog loading by using predefined EXTENSIONS instead of dynamic path loading

--- src/_setup_hook.py
+++ src/_setup_hook.py
@@ -1,41 +1,23 @@
 from logging import getLogger
-from pathlib import Path
 
 from discord.ext import commands
+
+from src.cogs import EXTENSIONS
 
 
 async def load_cogs(bot: commands.Bot) -> None:
     _logger = getLogger("luk.extensions")
 
-    async def path_loader(path: str = "./src/cogs") -> None:
-        for file in Path(path).iterdir():
-            if "__pycache__" in file.parts:
-                continue
+    for extension in EXTENSIONS:
+        _logger.debug("Loading extension %s", extension)
 
-            if file.is_dir():
-                _logger.debug("Loading extensions from %s", file)
+        try:
+            await bot.load_extension(extension)
+        except commands.NoEntryPointError:
+            _logger.warning(
+                "Extension %s has no setup function",
+                extension,
+            )
+            continue
 
-                await path_loader(str(file))
-                continue
-
-            if file.is_file() and (file.suffix != ".py" or file.name == "__init__.py"):
-                _logger.debug("Skipping non-Python file %s", file)
-                continue
-
-            cog_path = ".".join(file.parts).removesuffix(".py")
-            cog_short_path = ".".join(cog_path.split(".")[2:])
-
-            _logger.debug("Loading extension %s", cog_path)
-
-            try:
-                await bot.load_extension(cog_path)
-            except commands.NoEntryPointError:
-                _logger.warning(
-                    "Extension %s has no setup function",
-                    cog_short_path,
-                )
-                continue
-
-            _logger.info("Loaded extension %s", cog_short_path)
-
-    await path_loader()
+        _logger.info("Loaded extension %s", extension.split(".", 2)[-1])
