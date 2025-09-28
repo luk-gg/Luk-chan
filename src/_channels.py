060bbf3a05f265d3c41e525ba2ef9adcf6ab7fa4
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2025-09-28 05:25:23 -0300

    feat: implement _LukChannel and LukChannels classes for channel management

---
+++ src/_channels.py
@@ -0,0 +1,16 @@
+from src._settings import config
+
+
+class _LukChannel:
+    def __init__(self, id_: int) -> None:
+        self.id = id_
+        self.url = f"https://discord.com/channels/{config.GUILD_ID}/{id_}"
+
+    @staticmethod
+    def from_url(url: str) -> "_LukChannel":
+        channel_id = int(url.split("/")[-1])
+        return _LukChannel(channel_id)
+
+
+class LukChannels:
+    rules = _LukChannel(862607056132112394)
