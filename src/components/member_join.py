8b742ec701f2f3c2a417768bd97f29cfe9c7b674
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2025-09-28 04:56:07 -0300

    feat: Add MemberJoinView to display welcome message and resources for new members

---
+++ src/components/member_join.py
@@ -0,0 +1,41 @@
+from discord import ButtonStyle, Member, User, ui
+
+from src._colors import LukColors
+from src._emojis import LukEmojis
+
+
+class MemberJoinView(ui.LayoutView):
+    def __init__(self, user: Member | User) -> None:
+        super().__init__()
+
+        header = ui.TextDisplay["MemberJoinView"](
+            content="### Welcome to LUK.GG!",
+        )
+
+        text = ui.TextDisplay["MemberJoinView"](
+            content=(
+                "We're a growing community of theorycrafters, guide makers, "
+                "and video game enthusiasts! We welcome players of all skill levels "
+                "and play styles. Guides and resources can be found throughout this "
+                "server and on the website."
+            ),
+        )
+
+        user_icon = ui.Thumbnail["MemberJoinView"](media=user.display_avatar.url)
+
+        website_button = ui.ActionRow(
+            ui.Button["MemberJoinView"](
+                label="LUK.GG",
+                emoji=LukEmojis.luk_logo_rounded,
+                style=ButtonStyle.link,
+                url="https://luk.gg",
+            ),
+        )
+
+        self.add_item(
+            ui.Container(
+                ui.Section(header, text, accessory=user_icon),
+                website_button,
+                accent_color=LukColors.primary_blue,
+            ),
+        )
