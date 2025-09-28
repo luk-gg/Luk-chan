43e31d97d2080e83adf27445d4357eaba33a8280
Gabriel Nobrega <gabrieltkdnobrega63@gmail.com>
2025-09-28 03:33:10 -0300

    feat: Add LukEmojis class for managing Discord emojis

---
+++ src/_emojis.py
@@ -0,0 +1,32 @@
+from discord import PartialEmoji
+
+
+class LukEmojis:
+    # Roles
+    guest = PartialEmoji.from_str("<:role_guest:1421678555737227377>")
+    vip = PartialEmoji.from_str("<:role_vip:1421678139532247070>")
+    null = PartialEmoji.from_str("<:role_null:1421678131558744124>")
+    staff = PartialEmoji.from_str("<:role_staff:1421678122247524422>")
+    legend = PartialEmoji.from_str("<:role_legend:1421678113351536640>")
+    initiate = PartialEmoji.from_str("<:role_initiate:1421678100051267605>")
+    ascendant = PartialEmoji.from_str("<:role_ascendant:1421678087720014007>")
+
+    # Reactions
+    lukchan_wow = PartialEmoji.from_str("<:lukchan_wow:1421678029247221790>")
+    lukchan_sweat = PartialEmoji.from_str("<:lukchan_sweat:1421678018388168844>")
+    lukchan_noted = PartialEmoji.from_str("<:lukchan_noted:1421678005721501817>")
+    lukchan_nerd = PartialEmoji.from_str("<:lukchan_nerd:1421677995290001418>")
+    lukchan_cool = PartialEmoji.from_str("<:lukchan_cool:1421677985760809000>")
+
+    # Monsters
+    zakum_icon = PartialEmoji.from_str("<:zakum_icon:1421677908618907728>")
+
+    # Logos
+    luk_logo_transparent = PartialEmoji.from_str(
+        "<:luk_logo_transparent:1421677711696465951>",
+    )
+    luk_logo = PartialEmoji.from_str("<:luk_logo:1421677685649969172>")
+    luk_logo_rounded = PartialEmoji.from_str("<:luk_logo_rounded:1421677701353443428>")
+
+    bapharia_emblem = PartialEmoji.from_str("<:bapharia_emblem:1421678390938697808>")
+    bapharia_emblem2 = PartialEmoji.from_str("<:bapharia_emblem2:1421678407669780594>")
